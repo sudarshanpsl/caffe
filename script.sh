f82518eaa7e799726854d733dfd4ea55d9ed1a04
Sudarshan Test User <sudarshan_jagadale@persistent.com>
2017-01-17 19:54:47 +0530

    script file change - 3

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash  
-if [[ $(uname -m) == "ppc64le" ]]; then    
-  echo 'deb http://ftp.unicamp.br/pub/ppc64el/ubuntu/14_04/docker-1.6.2-ppc64el/ trusty main' | sudo tee /etc/apt/sources.list.d/trusty-docker.list    
-  sudo apt-get update    
-  sudo apt-get install -y --force-yes docker.io  
-fi  
-docker info  
-docker build -t test -f Dockerfile.`uname -m` .  
+if [[ $(uname -m) == "ppc64le" ]]; then 
+  echo 'deb http://ftp.unicamp.br/pub/ppc64el/ubuntu/14_04/docker-1.6.2-ppc64el/ trusty main' | sudo tee /etc/apt/sources.list.d/trusty-docker.list
+  sudo apt-get update
+  sudo apt-get install -y --force-yes docker.io
+fi
+docker info
+docker build -t test -f Dockerfile.`uname -m` .
 docker run -i -v $(pwd):/source2 --rm test
